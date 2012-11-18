54db8e4c7cc2f0592ed73fc49531c08cb7a7a2e7
Owain Perry <owain.perry@thetrainline.com>
2012-11-18 13:48:41 +0000

    Initial basic version

---
+++ src/SqlToGraphitePlugin-HttpPing/HttpPing.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using log4net;
+using SqlToGraphiteInterfaces;
+
+namespace SqlToGraphitePlugin_HttpPing
+{
+    public class HttpPing : PluginBase
+    {
+
+        public string Path { get; set; }
+
+        public string Proxy { get; set; }
+
+        public string Uri { get; set; }
+
+        public override string Name { get; set; }
+
+        public override string ClientName { get; set; }
+
+        public override string Type { get; set; }
+
+        public HttpPing()
+        {
+        }
+
+        public HttpPing(ILog log, Job job, IEncryption encryption)
+            : base(log, job, encryption)
+        {
+        }
+
+        private string HttpGet(string uri)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(uri);
+            if (!string.IsNullOrEmpty(Proxy))
+            {
+                req.Proxy = new System.Net.WebProxy(Proxy, false); //true means no proxy    
+            }
+            System.Net.WebResponse resp = req.GetResponse();
+            var sr = new System.IO.StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+
+        public override IList<IResult> Get()
+        {
+            var rtn = new List<IResult>();
+            try
+            {
+                Log.Debug(string.Format("HTTP-Get {0} ", Uri));                
+                var start = DateTime.Now;
+                var data = this.HttpGet(Uri);
+                var timetaken = DateTime.Now.Subtract(start);
+                var value = Convert.ToInt32(timetaken.TotalMilliseconds);
+                rtn.Add(new Result(value, Name, DateTime.Now, Path));
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex);
+                rtn.Add(new Result(-1, Name, DateTime.Now, Path));
+            }
+
+            return rtn;
+        }
+    }
+}
---
+++ test/SqlToGraphitePlugin-HttpPing.UnitTests/With_HttpPing.cs
@@ -0,0 +1,30 @@
+﻿using log4net;
+using NUnit.Framework;
+using Rhino.Mocks;
+using SqlToGraphiteInterfaces;
+
+namespace SqlToGraphitePlugin_HttpPing.UnitTests
+{
+    using System;
+
+    [TestFixture]
+    // ReSharper disable InconsistentNaming
+    public class With_HttpPing
+    {
+        [Test]
+        public void Should_Get_value_from_BBC_News()
+        {
+            var log = MockRepository.GenerateMock<ILog>();
+            var encryption = MockRepository.GenerateMock<IEncryption>();
+            Job job = new HttpPing();
+            var httpPing = new HttpPing(log, job, encryption);
+            httpPing.Uri = "http://www.bbc.co.uk/news";
+            //Test
+            var result = httpPing.Get();
+            //Assert
+            Assert.That(result.Count, Is.EqualTo(1));
+            Assert.That(result[0].Value, Is.GreaterThanOrEqualTo(10));
+            Console.WriteLine(result[0].Value);
+        }
+    }
+}
